6c71de390aabe3a67d76c527428843d022e7fee7
Skypex <84551201+Skyppex@users.noreply.github.com>
2024-04-11 00:32:01 +0200

    add tests for .. paths

--- src/main.rs
+++ src/main.rs
@@ -67,10 +67,19 @@ fn get_path(path: &str) -> Result<PathBuf>{
             dirs::home_dir().ok_or(std::io::Error::from(ErrorKind::NotFound))?.join(&p[2..])
         },
         p if p.starts_with("..") => {
-            get_path(std::env::current_dir()?.parent()
-                .ok_or(io::Error::from(ErrorKind::NotFound))?
-                .join(&p[3..]).to_str()
-                .ok_or(io::Error::from(ErrorKind::InvalidData))?)?
+            let current_dir = &std::env::current_dir()?;
+            let mut current_dir = current_dir.parent()
+                    .ok_or(std::io::Error::from(ErrorKind::NotFound))?;
+            
+            let mut path = Path::new(&p[3..]);
+
+            while path.starts_with("..") {
+                current_dir = current_dir.parent()
+                    .ok_or(std::io::Error::from(ErrorKind::NotFound))?;
+                path = Path::new(&path.to_str().unwrap()[3..]);
+            }
+
+            current_dir.join(path)
         },
         p if p.starts_with(".") => {
             std::env::current_dir()?.join(&p[2..])
@@ -101,6 +110,28 @@ mod tests {
         let binding = get_path(&path).unwrap();
         let full_path = binding.to_str().unwrap();
         let binding = Path::new("C:\\Users\\user\\.cargo").to_path_buf();
+        let expected = binding.to_str().unwrap();
+        assert_eq!(full_path, expected);
+    }
+
+    #[test]
+    fn test_get_double_dottet_path() {
+        let path = "..\\src".to_string();
+        let binding = get_path(&path).unwrap();
+        let full_path = binding.to_str().unwrap();
+        let binding = std::env::current_dir().unwrap()
+            .parent().unwrap().join("src");
+        let expected = binding.to_str().unwrap();
+        assert_eq!(full_path, expected);
+    }
+
+    #[test]
+    fn test_get_double_dottet_twice_path() {
+        let path = "..\\..\\src".to_string();
+        let binding = get_path(&path).unwrap();
+        let full_path = binding.to_str().unwrap();
+        let binding = std::env::current_dir().unwrap()
+            .parent().unwrap().parent().unwrap().join("src");
         let expected = binding.to_str().unwrap();
         assert_eq!(full_path, expected);
     }
